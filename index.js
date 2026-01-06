aa5e4200a12286eaf574c4ee943d16295037556a
choco-banana- <aitunikatu@outlook.com>
2026-01-06 14:31:34 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import http from 'node:http';
+import { createBareServer } from "@tomphttp/bare-server-node";
+import cors from 'cors';
+import path from 'node:path';
+
+const server = http.createServer();
+const app = express(server);
+const __dirname = process.cwd();
+const bareServer = createBareServer('/bare/');
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use exact string matching for now to avoid path-to-regexp errors in Express 5
+app.get('/scramjet', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+server.on('request', (req, res) => {
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeRequest(req, res);
+    } else {
+        app(req, res);
+    }
+});
+
+server.on('upgrade', (req, socket, head) => {
+    if (bareServer.shouldRoute(req)) {
+        bareServer.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
+});
+
+server.listen({ port: PORT }, () => {
+    console.log("Server listening on port " + PORT);
+});
+
+process.on("SIGINT", () => {
+    server.close();
+    bareServer.close();
+    process.exit(0);
+});
